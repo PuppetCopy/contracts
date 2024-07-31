4cfcb26372bbf34905ba1675a91316fdc6c50ca3
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2024-07-31 22:39:15 +0300

    refactor

--- src/token/VotingEscrow.sol
+++ src/token/VotingEscrow.sol
@@ -2,36 +2,35 @@
 pragma solidity 0.8.24;
 
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
-import {IAuthority} from "./../utils/interfaces/IAuthority.sol";
-import {Auth} from "./../utils/access/Auth.sol";
+import {IERC20Mintable} from "src/utils/interfaces/IERC20Mintable.sol";
+import {IAuthority} from "src/utils/interfaces/IAuthority.sol";
+import {Permission} from "src/utils/access/Permission.sol";
+import {Precision} from "src/utils/Precision.sol";
 
-import {Router} from "../shared/Router.sol";
+import {Router} from "src/shared/Router.sol";
 
-uint constant MAXTIME = 2 * 365 * 86400; // 2 years
+uint constant MAXTIME = 365 days * 2; // 1 year
 
 /**
- * @title Puppet Voting Escrow
- * @dev An ERC20 token with voting escrow capabilities.
- * The token is used to represent voting power in the governance system.
- * The token is minted by authiorized protocol contracts.
- * The token is non-transferable.
+ * @title Token Voting Escrow
+ * @dev A token that allows users to lock their tokens for a specified duration and receive a bonus
  */
-contract VotingEscrow is Auth, ERC20Votes {
-    event VotingEscrow__Lock(address depositor, address user, Lock lock);
-    event VotingEscrow__Withdraw(address user, address receiver, Lock lock);
-    event VotingEscrow__Release(address user, address receiver, Release release);
+contract VotingEscrow is Permission, ERC20Votes {
+    event VotingEscrow__Lock(address depositor, address user, uint duration, uint bonus, Lock lock);
+    event VotingEscrow__Vest(address user, address receiver, Vest vest);
+    event VotingEscrow__Claim(address user, address receiver, Vest vest, uint amount);
 
     struct Lock {
         uint amount;
         uint duration;
     }
 
-    struct Release {
+    struct Vest {
         uint amount;
         uint duration;
         uint lastSyncTime;
@@ -39,13 +38,15 @@ contract VotingEscrow is Auth, ERC20Votes {
     }
 
     mapping(address => Lock) public lockMap;
-    mapping(address => Release) public releaseMap;
+    mapping(address => Vest) public vestMap;
 
     Router public immutable router;
-    IERC20 public immutable token;
+    IERC20Mintable public immutable token;
 
-    constructor(IAuthority _authority, Router _router, IERC20 _token)
-        Auth(_authority)
+    uint public bonusMultiplier = 1e30;
+
+    constructor(IAuthority _authority, Router _router, IERC20Mintable _token)
+        Permission(_authority)
         ERC20("Puppet Voting Power", "vePUPPET")
         EIP712("Voting Escrow", "1")
     {
@@ -57,8 +58,8 @@ contract VotingEscrow is Auth, ERC20Votes {
         return lockMap[_user];
     }
 
-    function getRelease(address _user) external view returns (Release memory) {
-        return releaseMap[_user];
+    function getRelease(address _user) external view returns (Vest memory) {
+        return vestMap[_user];
     }
 
     function transfer(address, /*to*/ uint /*value*/ ) public pure override returns (bool) {
@@ -69,7 +70,8 @@ contract VotingEscrow is Auth, ERC20Votes {
         revert VotingEscrow__Unsupported();
     }
 
-    // lock minted tokens on behalf of the user for a specified duration
+    // lock deposited tokens on behalf of the user for a specified duration
+    // receive both governance tokens and bonus tokens based on the lock duration
     // the existing lock is updated with the new amount and duration weighted by the existing amount and duration
     function lock(
         address _depositor, //
@@ -77,73 +79,85 @@ contract VotingEscrow is Auth, ERC20Votes {
         uint _amount,
         uint _duration
     ) external auth {
-        if (_amount == 0 || _duration > MAXTIME) revert VotingEscrow__InvalidLock();
-
-        Lock memory _lock = lockMap[_user];
-
-        uint _lockedAmount = _lock.amount;
-        uint _nextBalance = _lockedAmount + _amount;
-        uint _nextDuration = (_lockedAmount * _lock.duration + _amount * _duration) / _nextBalance;
-
-        lockMap[_user] = Lock({amount: _nextBalance, duration: _nextDuration});
-        router.transfer(token, _depositor, address(this), _amount);
-        _mint(_user, _amount);
-
-        emit VotingEscrow__Lock(_depositor, _user, _lock);
-    }
-
-    // pool the user's locked tokens and release them to the receiver over time once release is called
-    // the existing release schedule is updated with the new amount and duration weighted by the existing amount and duration
-    function release(address _user, address _receiver, uint _amount) external auth {
         if (_amount == 0) revert VotingEscrow__ZeroAmount();
 
         Lock memory _lock = lockMap[_user];
-        Release memory _release = releaseMap[_user];
 
-        uint _nextBalance = _release.amount + _lock.amount;
-        uint _nextDuration = (_lock.amount * _lock.duration + _amount * _release.duration) / _nextBalance;
-        uint _emissionRate = _nextBalance / _nextDuration;
-
-        releaseMap[_user] = Release({
-            amount: _nextBalance,
-            duration: _nextDuration,
-            accrued: _release.accrued + ((block.timestamp - _release.lastSyncTime) * _emissionRate),
-            lastSyncTime: block.timestamp
-        });
-
-        _burn(_user, _amount);
-
-        emit VotingEscrow__Release(_user, _receiver, _release);
-    }
-
-    // withdraw the user's accrued tokens
-    function withdraw(address _user, address _receiver, uint _amount) external auth {
-        if (_amount == 0) revert VotingEscrow__ZeroAmount();
-        Release memory _release = releaseMap[_user];
-
-        uint _releaseRate = _release.amount / _release.duration;
-
-        releaseMap[_user] = Release({
-            amount: _release.amount, //
-            duration: _release.duration,
-            accrued: 0,
-            lastSyncTime: block.timestamp
-        });
-
-        token.transfer(_receiver, _release.accrued + ((block.timestamp - _release.lastSyncTime) * _releaseRate));
-    }
-
-    function stake(address _depositor, address _user, uint _amount) external auth {
         router.transfer(token, _depositor, address(this), _amount);
         _mint(_user, _amount);
+
+        uint _bonusToken = Precision.applyFactor(_amount, getLockRewardMultiplier(bonusMultiplier, _duration));
+        uint _totalAdded = _amount + _bonusToken;
+        token.mint(address(this), _bonusToken);
+
+        _lock.amount = _lock.amount + _totalAdded;
+        _lock.duration = (_lock.amount * _lock.duration + _totalAdded * _duration) / _lock.amount;
+        lockMap[_user] = _lock;
+
+        emit VotingEscrow__Lock(_depositor, _user, _duration, _bonusToken, _lock);
     }
 
-    function unstake(address _user, uint _amount) external auth {
-        token.transfer(_user, _amount);
+    // pool the user's locked tokens and vest them to the receiver over time once release is called
+    // the existing vesting schedule is averaged weighted whenever new amount and duration is added
+    function vest(address _user, address _receiver, uint _amount) external auth {
+        if (_amount == 0) revert VotingEscrow__ZeroAmount();
+
+        // vested tokens are excluded from governance voting power
         _burn(_user, _amount);
+
+        Lock memory _lock = lockMap[_user];
+
+        lockMap[_user] = Lock({amount: _lock.amount - _amount, duration: _lock.duration});
+
+        Vest memory _vest = getVestingCursor(_user);
+
+        _vest.amount = _vest.amount + _lock.amount;
+        _vest.duration = (_lock.amount * _lock.duration + _amount * _vest.duration) / _vest.amount;
+        vestMap[_user] = _vest;
+
+        emit VotingEscrow__Vest(_user, _receiver, _vest);
+    }
+
+    // claim the user's released tokens
+    function claim(address _user, address _receiver, uint _amount) external auth {
+        if (_amount == 0) revert VotingEscrow__ZeroAmount();
+        Vest memory _vest = getVestingCursor(_user);
+
+        _vest.accrued = _vest.accrued - _amount;
+        vestMap[_user] = _vest;
+
+        router.transfer(token, address(this), _receiver, _amount);
+
+        emit VotingEscrow__Claim(_user, _receiver, _vest, _amount);
+    }
+
+    function claimable(address _user) external view returns (uint) {
+        Vest memory _vest = vestMap[_user];
+        uint _timeElapsed = block.timestamp - _vest.lastSyncTime;
+        uint _emissionRate = _vest.amount / _vest.duration;
+
+        return _vest.accrued + Math.min(_vest.amount, (_timeElapsed * _emissionRate));
+    }
+
+    function getVestingCursor(address _user) internal view returns (Vest memory _vest) {
+        _vest = vestMap[_user];
+        uint _timeElapsed = block.timestamp - _vest.lastSyncTime;
+        uint _nextAccured = _vest.accrued + Math.min(_vest.amount, (_timeElapsed * (_vest.amount / _vest.duration)));
+
+        _vest.duration = _timeElapsed > _vest.duration ? 0 : _vest.duration - _timeElapsed;
+        _vest.accrued = _vest.accrued + _nextAccured;
+        _vest.amount = _vest.amount - _nextAccured;
+        _vest.lastSyncTime = block.timestamp;
+    }
+
+    function getLockRewardMultiplier(uint _bonusMultiplier, uint _lockDuration) public pure returns (uint) {
+        return Precision.applyFactor(_bonusMultiplier, Precision.toFactor(_lockDuration, MAXTIME));
+    }
+
+    function configBonusMultiplier(uint _bonusMultiplier) external auth {
+        bonusMultiplier = _bonusMultiplier;
     }
 
     error VotingEscrow__ZeroAmount();
-    error VotingEscrow__InvalidLock();
     error VotingEscrow__Unsupported();
 }
