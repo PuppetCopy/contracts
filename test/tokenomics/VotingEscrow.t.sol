d711725af24b5d4a4000042f18adeaeba4636d38
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2024-09-06 00:22:36 +0300

    Update MAXTIME constant to 106 weeks for VotingEscrowLogic and related tests

--- test/tokenomics/VotingEscrow.t.sol
+++ test/tokenomics/VotingEscrow.t.sol
@@ -8,7 +8,7 @@ import {VotingEscrowStore} from "src/tokenomics/store/VotingEscrowStore.sol";
 import {BasicSetup} from "test/base/BasicSetup.t.sol";
 
 contract VotingEscrowTest is BasicSetup {
-    uint constant MAXTIME = 110 weeks; // about 2 years
+    uint constant MAXTIME = 106 weeks; // about 2 years
 
     VotingEscrowStore veStore;
     VotingEscrowLogic veLogic;
